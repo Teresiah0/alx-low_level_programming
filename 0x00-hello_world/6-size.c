e79ac66b4ce2fa52e9a67942f6c31945bb28c299
Teresiah0 <machariat07@gmail.com>
2023-03-16 13:13:46 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@ int main(void)
 int num [] = {1, 4, 8};
 printf("Size of a char: %d byte(s)\n", num[0]);
 printf("Size of an int: %d byte(s)\n", num[1]);
-printf("Size of a long int: %d byte(s)\n", num[1]);
+printf("Size of a long int: %d byte(s)\n", num[2]);
 printf("Size of a long long int: %d byte(s)\n", num[2]);
 printf("Size of a float: %d byte(s)\n", num[1]);
 return (0);
