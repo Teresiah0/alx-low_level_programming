bd3b50a1aef3818e980e3c0895ac8349f37659f3
Teresiah0 <machariat07@gmail.com>
2023-03-21 15:17:26 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - returning absoulte value of number
+ *
+ * @i: number to get absolute
+ *
+ * Return: absolute value of i
+ */
+int _abs(int i)
+{
+	if (i < 0)
+	{
+		return (-i);
+	}
+	return (i);
+}
