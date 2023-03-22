a26be09bd631dabc73d3f86fc193941c0b004e77
Teresiah0 <machariat07@gmail.com>
2023-03-21 19:21:33 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - multiplication table
+ *
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int col, row;
+
+	for (row = 0 ; row < 10 ; row++)
+	{
+	for (col = 0 ; col < 10 ; col++)
+	{
+		if (col != 0)
+		{
+		if ((row * col) < 10)
+		{
+		_putchar(' ');
+		}
+		_putchar(' ');
+		}
+		if (row * col >= 10)
+		{
+		_putchar('0' + (row * col) / 10);
+		_putchar('0' + (row * col) % 10);
+		}
+		else
+		{
+		_putchar('0' + (row * col));
+		}
+		if (col  != 9)
+		{
+		_putchar(',');
+		}
+	}
+	_putchar('\n');
+	}
+}
